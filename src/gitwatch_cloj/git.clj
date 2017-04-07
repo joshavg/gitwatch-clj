9ac0bf403e2a7f125cb94051ea4cf1a2b8ac808a
Josha von Gizycki <josha.gizycki@triology.de>
2017-04-07 14:38:45 +0200

    nicer code

--- src/gitwatch_cloj/git.clj
+++ src/gitwatch_cloj/git.clj
@@ -14,12 +14,12 @@
                        (File. path)))]
         [(Git. repo) repo]))
 
-(defn status-git
-    [repo-parsed show-unchanged]
+(defn fetch-status
+    [repo-instances show-unchanged]
     (try
-        (let [git      (:git repo-parsed)
-              repo     (:repo repo-parsed)
-              name     (:name repo-parsed)
+        (let [git      (:git repo-instances)
+              repo     (:repo repo-instances)
+              name     (:name repo-instances)
               branch   (.getBranch repo)
               status   (.call (.status git))
               is-clean (.isClean status)
@@ -32,30 +32,33 @@
              :ahead    (or ahead "?")
              :_show    (or show-unchanged (not is-clean) (> (or ahead 1) 0))})
         (catch NoWorkTreeException e
-               {:name     (:name repo-parsed)
+               {:name     (:name repo-instances)
                 :branch   "check path"
                 :clean    "?"
                 :modified "?"
                 :ahead    "?"
                 :_show    true})))
 
+(defn table-content
+    [config show-unchanged]
+    (map
+        #(fetch-status % show-unchanged)
+        (map
+            (fn [[repo-name path]]
+                (let [[git repo] (create-git path)]
+                    {:name (name repo-name)
+                     :git  git
+                     :repo repo}))
+            (:repos config))))
+
 (defn status-table
     [config show-unchanged]
     (with-out-str
         (print-table
             [:name :branch :clean :modified :ahead]
-            (filter #(:_show %)
-                    (map
-                        #(status-git % show-unchanged)
-                        (map
-                            (fn [repo-entry]
-                                (let [path       (val repo-entry)
-                                      repo-name  (key repo-entry)
-                                      [git repo] (create-git path)]
-                                    {:name (name repo-name)
-                                     :git  git
-                                     :repo repo}))
-                            (:repos config)))))))
+            (sort-by :name
+                     (filter :_show
+                             (table-content config show-unchanged))))))
 
 (defn status-changed [config]
     (status-table config false))
